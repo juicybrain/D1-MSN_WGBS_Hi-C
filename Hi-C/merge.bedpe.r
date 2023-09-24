57231c8ee9501e8256654a12e9df617a7afe210a
juicybrain <97708281+juicybrain@users.noreply.github.com>
2023-09-23 22:56:30 -0400

    Create merge.bedpe.r

---
+++ Hi-C/merge.bedpe.r
@@ -0,0 +1,6 @@
+    library(hictoolsr)
+    library("dbscan")
+        loop_25k_files = c("file1", "file2")
+
+        write.table( dat_loop_25k, "dat_loop_25k.bedpe", quote=F, sep="\t", row.names=F)
+       
